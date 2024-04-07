8ffb7b2ab9e999eed7997abb626911c7b39ff16d
e1michi <kaz@takahashi.com>
2024-04-07 18:17:06 +0000

    production用対処。

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
-import GoogleAnalytics from "@/components/google_analytics";
-import { Suspense } from "react";
+import { GTM_ID } from "@/lib/gtm";
+import Script from "next/script";
 
 /**
  * 利用するフォントを定義する。
@@ -40,9 +40,30 @@ export default function RootLayout({
         text-base
         text-gray-700
       `}>
-        <Suspense>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        <Script
+          id="gtm-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer', '${GTM_ID}');
+                    `,
+          }}
+        />
+        {/* <Suspense>
           <GoogleAnalytics />
-        </Suspense>
+        </Suspense> */}
         <div id="header" className="relative z-20 h-20">
           <div className="fixed top-0 w-full bg-white">
             <Header />
--- components/google_analytics.tsx
+++ components/google_analytics.tsx
@@ -27,7 +27,7 @@ export default function Analytics() {
             </noscript>
             <Script
                 id="gtm-script"
-                strategy="afterInteractive"
+                strategy="beforeInteractive"
                 dangerouslySetInnerHTML={{
                     __html: `
                         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':

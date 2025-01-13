1b8224306f2fd17dbb0782242aba332e539900d1
e1michi <kaz@takahashi.com>
2025-01-13 07:57:48 +0000

    サービスリンクを追加

--- app/page.tsx
+++ app/page.tsx
@@ -73,6 +73,34 @@ export default async function Home() {
         <Cover pathname='/' />
         <div className="container mx-auto px-6 md:px-20">
           <div className="my-10">
+            <div className="flex flex-col">
+              <div className="text-2xl font-semibold">
+                &mdash; OUR SERVICES &mdash;
+              </div>
+              <div className="mt-4 flex flex-row items-center gap-x-4">
+                <a href="https://ecopluspc.cyberedge.jp" target="_blank" className="flex flex-row items-center">
+                  <img src="/ECOPLUSPC.png" className="size-12" />
+                  <span className="text-xl">
+                    法人向けPCリユース
+                  </span>
+                </a>
+                <a href="https://www.sanpou-yoshi.co.jp" target="_blank" className="flex flex-row items-center">
+                  <img src="/SANPOU-YOSHI.webp" className="size-12" />
+                  <span className="text-xl">
+                    会計事務所向け業務支援ツール
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container mx-auto px-6 md:px-20">
+          <div className="my-10">
+            <div className="flex flex-col">
+              <div className="text-2xl font-semibold">
+                &mdash; NEWS &mdash;
+              </div>
+            </div>
             {user_events.data.map((entity: UserEventEntity) => {
               return create_event_element(entity);
             })}
--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <footer className="text-white bg-gray-800">
             <div className="flex justify-center items-center py-4">
-                @ 2020 CYBEREDGE Inc.
+                &copy; 2020 CYBEREDGE Inc.
             </div>
         </footer>
     );
